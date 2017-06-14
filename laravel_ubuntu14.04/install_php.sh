46aaca342aca20bb27a0bae5e99de11c6b992bd3
nicklian <w19900227@gmail.com>
2017-06-14 09:28:22 +0000

    temp

--- laravel_ubuntu14.04/install_php.sh
+++ laravel_ubuntu14.04/install_php.sh
@@ -1,6 +1,8 @@
-sudo apt-get remove "php*"
+sudo apt-get remove -y "php*"
+#sudo apt-get install python-software-properties
 sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
 sudo apt-get install -y php5.6 php5.6-fpm php5.6-cli php5.6-mcrypt git
+#sudo apt-get install -y php5.6-gd
 sudo cp files/php.ini /etc/php5/fpm/php.ini
 sudo service php5.6-fpm restart
